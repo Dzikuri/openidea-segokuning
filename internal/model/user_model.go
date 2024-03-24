2b585d5f28763a894e5b54ebf9b6e794d3388298
Dzikri Aris F <dzikri.developer@gmail.com>
2024-03-24 07:32:51 +0700

    minor bug fix

--- internal/model/user_model.go
+++ internal/model/user_model.go
@@ -69,8 +69,7 @@ type UserUpdateAccount struct {
 func (p UserUpdateAccount) Validate() error {
 	return validation.ValidateStruct(&p,
 		validation.Field(&p.Name, validation.Required, validation.Length(5, 50)),
-		validation.Field(&p.ImageUrl, validation.Required, validation.Match(regexp.MustCompile(`^(https?|ftp):\/\/[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(\/\S*)?$
-`))),
+		validation.Field(&p.ImageUrl, validation.Required, validation.Match(regexp.MustCompile(`^(https?|ftp):\/\/[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(\/\S*)?$`))),
 	)
 }
 
